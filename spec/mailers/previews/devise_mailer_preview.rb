15f238ea7b71b38f48fef2b6e13b746fc2f0d344
Crystian Leão <crystian@roadmaps.com.br>
2021-03-20 11:55:40 -0300

    Fixed notifications widget, changed mails and default theme colors

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
+# Preview this emails at http://localhost:3000/rails/mailers/devise_mailer
 class DeviseMailerPreview < ActionMailer::Preview
 
   def confirmation_instructions
-    DeviseMailer.confirmation_instructions(User.first, "faketoken", {})
+    DeviseMailer.confirmation_instructions(User.first, 'faketoken', {})
   end
 
   def reset_password_instructions
-    DeviseMailer.reset_password_instructions(User.first, "faketoken", {})
+    DeviseMailer.reset_password_instructions(User.first, 'faketoken', {})
   end
 
   def unlock_instructions
-    DeviseMailer.unlock_instructions(User.first, "faketoken", {})
+    DeviseMailer.unlock_instructions(User.first, 'faketoken', {})
   end
 end
