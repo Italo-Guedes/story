b21ef785d3efa37c8813ad91ce5c3e578be10955
kelvyn pereira <kelvyn@roadmaps.com.br>
2023-03-30 14:25:46 -0300

    🔒 chore(rack_attack.rb): add comments and remove unused code and change comments to pt-br

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,87 +1,57 @@
 class Rack::Attack
 
   ### Configure Cache ###
-
-  # If you don't want to use Rails.cache (Rack::Attack's default), then
-  # configure it here.
+  # Implementa uma interface de armazenamento de cache
   #
-  # Note: The store is only used for throttling (not blocklisting and
-  # safelisting). It must implement .increment and .write like
-  # ActiveSupport::Cache::Store
-
+  # ActiveSupport::Cache::Store 
   # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
-  ### Configure Blocklist ###
+  ###  Configure Blocklist ###
+  # O método pode ser usado para bloquear IPs específicos
+  # que estejam tentando acessar o servidor.
+  #
   # Rack::Attack.blocklist_ip("")
 
-  ### Throttle Spammy Clients ###
-
-  # If any single client IP is making tons of requests, then they're
-  # probably malicious or a poorly-configured scraper. Either way, they
-  # don't deserve to hog all of the app server's CPU. Cut them off!
-  #
-  # Note: If you're serving assets through rack, those requests may be
-  # counted by rack-attack and this throttle may be activated too
-  # quickly. If so, enable the condition to exclude them from tracking.
-
-  # Throttle all requests by IP (60rpm)
+  #### Throttle all requests by IP (60rpm) ### 
+  # Limita a taxa de requisições, o que significa que um único endereço
+  # IP só pode fazer 60 requisições por minuto antes de ser
+  # limitado pelo mecanismo de throttle
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.ip # unless req.path.start_with?('/assets')
-  end
+  # throttle('req/ip', limit: 300, period: 5.minutes) do |req|
+  #   req.ip
+  # end
 
   ### Prevent Brute-Force Login Attacks ###
-
-  # The most common brute-force login attack is a brute-force password
-  # attack where an attacker simply tries a large number of emails and
-  # passwords to see if any credentials match.
+  # Bloqueia requisições POST para /login que excedam um limite
+  # de 5 requisições por 20 segundos por endereço IP.
   #
-  # Another common method of attack is to use a swarm of computers with
-  # different IPs to try brute-forcing a password for a specific account.
-
-  # Throttle POST requests to /login by IP address
-  #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/ip:#{req.ip}"
   throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
     if req.path == '/users/sign_in' && req.post?
       req.ip
     end
   end
 
-  # Throttle POST requests to /login by email param
+  ### Throttle POST requests to /login by email param ### 
+  # Essa configuração de throttle limita o número de requisições
+  # POST que um usuário pode fazer para a rota /login
+  # com base no valor do parâmetro "email".
   #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/email:#{normalized_email}"
-  #
-  # Note: This creates a problem where a malicious user could intentionally
-  # throttle logins for another user and force their login requests to be
-  # denied, but that's not very common and shouldn't happen to you. (Knock
-  # on wood!)
   throttle('logins/email', limit: 5, period: 20.seconds) do |req|
     if req.path == '/users/sign_in' && req.post?
-      # Normalize the email, using the same logic as your authentication process, to
-      # protect against rate limit bypasses. Return the normalized email if present, nil otherwise.
       req.params['user']['email'].to_s.downcase.gsub(/\s+/, "").presence
     end
   end
 
   ### Custom Throttle Response ###
-
-  # By default, Rack::Attack returns an HTTP 429 for throttled responses,
-  # which is just fine.
+  # Método para personalizar a resposta do servidor
+  # quando um usuário excede o limite de requisições
+  # definido pelo mecanismo de throttle
+  # (ou seja, de limitação de requisições).
   #
-  # If you want to return 503 so that the attacker might be fooled into
-  # believing that they've successfully broken your app (or you just want to
-  # customize the response), then uncomment these lines.
-  self.throttled_responder = lambda do |env|
-    # NB: you have access to the name and other data about the matched throttle
-    #  request.env['rack.attack.matched'],
-    #  request.env['rack.attack.match_type'],
-    #  request.env['rack.attack.match_data'],
-    #  request.env['rack.attack.match_discriminator']
-
-    # Using 503 because it may make attacker think that they have successfully
-    # DOSed the site. Rack::Attack returns 429 for throttling by default
-    [ 503, {}, ["Server Error\n"]]
-  end
+  # self.throttled_responder = lambda do |env|
+  #  [ 503,  # status
+  #    {},   # headers
+  #    ['']] # body
+  # end
 end
